ca1068a8cad9866d24cc14d22eba42fad7dbcd05
elongberg <elongberg@gmail.com>
2015-09-03 13:55:47 -0600

    Started project. Comments and basic function outlines.

---
+++ PS1/FormulaEvaluator/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// Eric Longberg
+/// CS 3500, PS1
+/// September 10, 2015
+namespace FormulaEvaluator
+{
+    /// <summary>
+    /// Evaluator of standard infix notation.
+    /// Supports the operators +, -, *, / and allows for parenthesis,
+    /// non-negative integers, and variables consisting of 1+ letters
+    /// followed by 1+ digits. Whitespace is ignored.
+    /// </summary>
+    public static class Evaluator
+    {
+        public delegate int Lookup(String v);
+
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            string[] substrings = System.Text.RegularExpressions.Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            return 1;
+        }
+    }
+}
