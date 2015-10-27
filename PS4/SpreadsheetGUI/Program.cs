4a8764c9792945361b4e80d0b27d52e5feafc192
elongberg <elongberg@gmail.com>
2015-10-27 00:46:59 -0600

    Basic layout of project and skeleton methods, non-functional GUI elements, name changes.

--- PS4/SpreadsheetGUI/Program.cs
+++ PS4/SpreadsheetGUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,7 +19,60 @@ namespace SpreadsheetGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            // Start an application context and run one form inside it
+            SpreadsheetAppContext context = SpreadsheetAppContext.getAppContext();
+            context.RunForm(new SpreadsheetForm());
+            Application.Run(context);
+
         }
+    }
+
+
+    /// <summary>
+    /// Keeps track of the current spreadsheet windows.
+    /// </summary>
+    class SpreadsheetAppContext : ApplicationContext
+    {
+        // Number of open windows
+        private int windowCount = 0;
+
+        // Singleton ApplicationContext
+        private static SpreadsheetAppContext appContext;
+
+        /// <summary>
+        /// Private constructor for singleton pattern
+        /// </summary>
+        private SpreadsheetAppContext()
+        {
+        }
+
+        /// <summary>
+        /// Returns the one SpreadsheetApplicationContext.
+        /// </summary>
+        public static SpreadsheetAppContext getAppContext()
+        {
+            if (appContext == null)
+            {
+                appContext = new SpreadsheetAppContext();
+            }
+            return appContext;
+        }
+
+        /// <summary>
+        /// Runs the form
+        /// </summary>
+        public void RunForm(Form form)
+        {
+            // One more form is running
+            windowCount++;
+
+            // When this form closes, we want to find out
+            form.FormClosed += (o, e) => { if (--windowCount <= 0) ExitThread(); };
+
+            // Run the form
+            form.Show();
+        }
+
     }
 }
